98a9f5b4ba8716b611119ce479971ac348275d3b
DESKTOP-5N53M1Q\web-05 <pankajk@zoptal.in>
2023-07-25 16:11:51 +0530

    yes

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+
+app.listen(5010, () => {
+    console.log("server is running on 5010")
+})
